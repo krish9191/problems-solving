b1bbe38eebafc796f18810ec5c7fa900b8cdcb5c
krishna <contact.krishnatiwari@gmail.com>
2021-05-21 01:29:08 +0200

    count occurence of the substring

--- check_substring.py
+++ check_substring.py
@@ -1,14 +1,14 @@
 def count_substring(string, sub_string):
-    count = 0
+    count_string = 0
     i = string.find(sub_string)
     while i != -1:
-        count += 1
-        i = string.find(sub_string, i+1)
-    return count
+        count_string += 1
+        i = string.find(sub_string, i + 1)
+    return count_string
+
 
 if __name__ == '__main__':
-    string =input()
-    sub_string =input()
-
-    count = count_substring(string, sub_string)
+    enter_string = input("enter a string:")
+    enter_sub_string = input("enter a sub_string:")
+    count = count_substring(enter_string, enter_sub_string)
     print(count)
