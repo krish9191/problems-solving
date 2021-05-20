2e2180e1edfe2b4c4ca4a6f0630770ff1c13615f
krishna <contact.krishnatiwari@gmail.com>
2021-05-21 01:01:49 +0200

    insertion sort

--- Kwargs.py
+++
@@ -1,9 +0,0 @@
-def info(**data):
-    for key, value in data.items():
-        print("{} is {}".format(key, value))
-
-
-if __name__ == '__main__':
-    info(firstname='rajesh', lastname='khadka', age=28, address='93 rue talma')
-
-    info(firstname='krishna', lastname='tiwari', age=29, address='12 rue racine')
--- sort_array.py
+++ sort_array.py
@@ -1,12 +1,10 @@
-def array_sort():
-    arr_1 = [85, 5,21, 82, 54,17,15 ]
-
+def sort_array():
+    arr_1 = [85, 5, 21, 82, 54, 17, 15]
     for i in range(len(arr_1)):
-        for j in range(1,len(arr_1)):
+        for j in range(1, len(arr_1)):
             if arr_1[j] <= arr_1[j - 1]:
                 arr_1[j], arr_1[j - 1] = arr_1[j - 1], arr_1[j]
-
     print(arr_1)
 
 
-array_sort()
+sort_array()
