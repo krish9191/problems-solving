c16900ea433ce8c6c50a26fa6771d3639d87dac7
krishna <contact.krishnatiwari@gmail.com>
2021-05-21 01:45:17 +0200

    calculate fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,16 +1,13 @@
 def fibonacci(n):
-
-    if n == 0:
-        return 0
-    a=0
-    b=1
-    for i in range(n-1):
-        c=a+b
-        a=b
-        b=c
-
+    a = 0
+    b = 1
+    c = 0
+    for i in range(n - 1):
+        c = a + b
+        a = b
+        b = c
     return c
 
-if __name__ =='__main__':
-   x= fibonacci(7)
-   print(x)
+
+x = fibonacci(4)
+print(x)
