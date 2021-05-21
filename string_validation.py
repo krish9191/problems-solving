ea03e68bc29422bd68cffcc83dcdcfb0983108a3
krishna <contact.krishnatiwari@gmail.com>
2021-05-21 13:40:27 +0200

    delete files

--- string_validation.py
+++ string_validation.py
@@ -1,13 +1,6 @@
 if __name__ == '__main__':
     s = 'AABcc'
 
-
-    # print(any(char.isalnum() for char in s))
-    # print(any(char.isalpha() for char in s))
-    # print(any(char.isdigit() for char in s))
-    # print(any(char.islower() for char in s))
-    # print(any(char.isupper() for char in s))
-
-    lf = [str.isalnum, str.isalpha,str.isdigit, str.islower, str.isupper]
+    lf = [str.isalnum, str.isalpha, str.isdigit, str.islower, str.isupper]
     for item in lf:
         print(any(item(c) for c in s))
