aebf781d50cce9149410bf4bf6e76d6b17cde014
krishna <contact.krishnatiwari@gmail.com>
2021-05-21 12:19:24 +0200

    sort an array

--- sort_array.py
+++ sort_array.py
@@ -1,10 +1,10 @@
-def sort_array():
-    arr_1 = [85, 5, 21, 82, 54, 17, 15]
-    for i in range(len(arr_1)):
-        for j in range(1, len(arr_1)):
-            if arr_1[j] <= arr_1[j - 1]:
-                arr_1[j], arr_1[j - 1] = arr_1[j - 1], arr_1[j]
-    print(arr_1)
+def sort_array(arr):
+    for i in range(len(arr)):
+        for j in range(1, len(arr)):
+            if arr[j] <= arr[j - 1]:
+                arr[j], arr[j - 1] = arr[j - 1], arr[j]
+    return arr
 
 
-sort_array()
+enter_arr = [45, 78, 3, 65, 96]
+print(sort_array(enter_arr))
