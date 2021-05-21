7eb401542faaa5f250450d2be81a4d38e921301e
krishna <contact.krishnatiwari@gmail.com>
2021-05-21 10:22:00 +0200

    it prints the Bowtie pattern created with letter 'H'

--- loop_pattern.py
+++ loop_pattern.py
@@ -1,20 +1,19 @@
-
 for i in range(5):
     for j in range(5-i):
-        print(' ',end='')
+        print(' ', end='')
     for j in range(i):
-        print('H',end='')
+        print('H', end='')
     for j in range(i):
-        print('H',end='')
+        print('H', end='')
     print('H')
 for i in range(1):
     for j in range(1):
         print('   HHHHH')
 for i in range(5):
     for j in range(i+1):
-        print(' ',end='')
+        print(' ', end='')
     for j in range(4-i):
-        print('H',end='')
+        print('H', end='')
     for j in range(4-i):
-        print('H',end='')
+        print('H', end='')
     print('H')
