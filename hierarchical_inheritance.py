27ba33483cbeb5fcb2d14a0fdb157cc4ba35733e
krishna <contact.krishnatiwari@gmail.com>
2021-05-21 02:04:08 +0200

    payroll system for different payment method

--- hierarchical_inheritance.py
+++ hierarchical_inheritance.py
@@ -1,53 +1,52 @@
+class Employee:
+    def __init__(self, emp_id, name):
+        self.id = emp_id
+        self.name = name
 
 
-class Employees:
-    def __init__(self,id ,name):
-        self.id=id
-        self.name=name
-
-class Payrollsystem:
-    def payroll_calculator(self,Employees):
-        for employee in Employees:
+class PayrollSystem:
+    @classmethod
+    def payroll_calculator(cls, Employee):
+        for employee in Employee:
             print(f'Id:{employee.id}-{employee.name}')
 
             print(f'salary amount:{employee.payroll_calculator()}')
 
-class Salaryemployees(Employees):
-    def __init__(self,id,name,monthly_salary):
-        super().__init__(id,name)
-        self.salary=monthly_salary
+
+class SalaryEmployee(Employee):
+    def __init__(self, emp_id, name, monthly_salary):
+        super().__init__(emp_id, name)
+        self.salary = monthly_salary
+
     def payroll_calculator(self):
         return self.salary
 
-class Hourlyemployees(Employees):
-    def __init__(self,id,name,no_of_hours,wage_per_hours):
-        super().__init__(id,name)
-        self.hours=no_of_hours
-        self.wage=wage_per_hours
-    def payroll_calculator(self):
-        return self.hours*self.wage
 
-class Commissionemployees(Salaryemployees):
-    def __init__(self,id,name,monthly_salary,commision):
-        super().__init__(id,name,monthly_salary)
-        self.commission=commision
-    def payroll_calculator(self):
-        fixed=super().payroll_calculator()
-        return fixed+self.commission
+class HourlyEmployees(Employee):
+    def __init__(self, emp_id, name, no_of_hours, wage_per_hours):
+        super().__init__(emp_id, name)
+        self.hours = no_of_hours
+        self.wage = wage_per_hours
 
-if __name__=='__main__':
-    s=Salaryemployees('00kt','krishna',5000)
-    h=Hourlyemployees('00rp','ramesh',80,25)
-    c=Commissionemployees('00sp','sagar',2000,500)
-    payroll=Payrollsystem()
+    def payroll_calculator(self):
+        return self.hours * self.wage
+
+
+class CommissionEmployees(SalaryEmployee):
+    def __init__(self, emp_id, name, monthly_salary, commission):
+        super().__init__(emp_id, name, monthly_salary)
+        self.commission = commission
+
+    def payroll_calculator(self):
+        fixed = super().payroll_calculator()
+        return fixed + self.commission
+
+
+if __name__ == '__main__':
+    s = SalaryEmployee('00kt', 'Krishna', 5000)
+    h = HourlyEmployees('00rp', 'Ramesh', 80, 25)
+    c = CommissionEmployees('00sp', 'Santosh', 2000, 500)
+    payroll = PayrollSystem()
     payroll.payroll_calculator([
-        s, h,c
+        s, h, c
     ])
-
-
-
-
-
-
-
-
